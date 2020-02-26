bf7837e89818db483a1822697c690866ab7e823c
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-02-26 13:57:55 -0600

    method demo

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -6,29 +6,36 @@ namespace ReverseString
     {
         static void Main(string[] args)
         {
+            // a variable always has exactly one declaration(where you give it its type)
+            // and that variable is "in scope" (accessible) from that line until the next "}"
+
             Console.Write("Input a string: ");
             string input = Console.ReadLine();
             Reverse(input);
         }
 
-        static void Reverse(string input)
+        static void Reverse(string data)
         {
-            int length = input.Length;
+            // "input" in here, is an error
+            // because that variable is not in scope.
+            // C# keeps things organized this way.
+            int length = data.Length;
             string reversedString = "";
+
             for (int i = length - 1; i >= 0; i--)
             {
-                reversedString += input[i];
+                reversedString += data[i];
             }
             Console.WriteLine("Reversed String: " + reversedString);
         }
 
-        static void Reverse2(string input)
-        {
-            string exString = "abc.123.def.456";
-            char[] exCharArray = exString.ToCharArray();
-            Array.Reverse(exCharArray);
-            string revExString = String.Concat(exCharArray);
-            Console.WriteLine(revExString);
-        }
+        // static void Reverse2(string input)
+        // {
+        //     string exString = "abc.123.def.456";
+        //     char[] exCharArray = exString.ToCharArray();
+        //     Array.Reverse(exCharArray);
+        //     string revExString = String.Concat(exCharArray);
+        //     Console.WriteLine(revExString);
+        // }
     }
 }
---
+++ ReverseStringWithoutMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReverseStringWithoutMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a string: ");
+            string input = Console.ReadLine();
+
+            string data = input;
+
+            int length = data.Length;
+            string reversedString = "";
+
+            for (int i = length - 1; i >= 0; i--)
+            {
+                reversedString += data[i];
+            }
+            Console.WriteLine("Reversed String: " + reversedString);
+        }
+    }
+}
