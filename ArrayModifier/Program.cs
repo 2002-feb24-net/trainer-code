9270d23068cef241d9259b4254c7c03a8620ee17
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-02-26 11:02:41 -0600

    add skeleton arraymodifier program.

---
+++ ArrayModifier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ArrayModifier
+{
+    class Program
+    {
+        // accept input from user, list of numbers separated by spaces
+        // interpret that as an int array
+        // print the array's values back to the user
+        // ask the user for some operation
+        // print the array's new values to the user.
+        static void Main(string[] args)
+        {
+            string input = GetInput();
+            int[] array = InterpretStringAsArray(input);
+            PrintArray(array);
+        }
+
+        static void PrintArray(int[] a)
+        {
+
+        }
+
+        static int[] InterpretStringAsArray(string str)
+        {
+
+        }
+
+        // any method is going to have
+        // 1. a name
+        // 2. a return value: either nothing (void), or some type e.g. int
+            // what result does the method need to send back to the code that uses this method.
+        // 3. a list of parameters (maybe empty)
+            // does the method need any input from the code that uses it to do its job.
+        static string GetInput()
+        {
+
+        }
+
+        static int Add(int a, int b)
+        {
+            int result = a + b;
+            return result;
+            // you use "return" to send the return value back to the
+            // code that calls this method.
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // arrays are the most fundamental/basic way to
+            // have a collection/sequence of data in C#.
+
+            // the main rules about them:
+            // 1) once they've been created, they can't get larger or smaller
+            // 2) you can only hold one type of thing in a given array
+
+            // ways to make an array:
+            // 1) get it from some built-in method, e.g.
+            string[] words = "a sentence".Split(' ');
+            // 2) make it with default values
+            int[] numbers = new int[8]; // length 8, all starting at zero.
+            // 3) array initializer
+            int[] moreNumbers = { 1, 6, 4, 9, int.Parse("12") }; // length 5, with given initial values
+
+            // we can access any value by index
+            int firstValue = moreNumbers[0];
+            // just like strings, arrays indexes start at 0 and go up to Length - 1.
+            moreNumbers[1] = 5; // set the second value to 5.
+
+            int length = moreNumbers.Length;
+            int[] newCopy = new int[moreNumbers.Length];
+            Array.Copy(moreNumbers, newCopy, length);
+
+            int index = Array.IndexOf(moreNumbers, 3); // index of first 3 found in array.
+        }
+    }
+}
