b3dffe4146af1931fa73bbd1ac11d0358e29729f
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-02-26 09:32:53 -0600

    exercise 2 solns.

---
+++ Day2Exercise2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day2Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a staircase height: ");
+            string input = Console.ReadLine();
+            int length = int.Parse(input);
+            for (int i = 0; i <= length; i++)
+            {
+                // debug print
+                // Console.WriteLine(i);
+
+                // solution 1
+                // Console.WriteLine(new String(' ', length - i) + new String('#', i));
+
+                // solution 2
+                // for (int j = 0; j < length - i; j++)
+                // {
+                //     Console.Write(" ");
+                // }
+                // for (int j = 0; j < i; j++)
+                // {
+                //     Console.Write("#");
+                // }
+                // Console.WriteLine(); // line break
+
+                // solution 3
+                string stairs = new String('#', i);
+                Console.WriteLine(stairs.PadLeft(length));
+            }
+        }
+    }
+}
