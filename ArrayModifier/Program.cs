8ebe0dfc094af5198e5b18587481f6ef75af4851
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-02-26 11:38:13 -0600

    implement two methods.

--- ArrayModifier/Program.cs
+++ ArrayModifier/Program.cs
@@ -16,27 +16,38 @@ namespace ArrayModifier
             PrintArray(array);
         }
 
+        // given any integer array,
+        // print out all its values
         static void PrintArray(int[] a)
         {
+            Console.Write("Array contents: ");
 
+            for (int i = 0; i < a.Length; i++)
+            {
+
+                Console.Write(a[i].ToString() + " ");
+            }
+            Console.WriteLine();
         }
 
         static int[] InterpretStringAsArray(string str)
         {
+        }
 
+        static string GetInput()
+        {
+            Console.WriteLine("Enter a list of whole numbers separated by spaces.");
+            Console.WriteLine("e.g.: '12 3 -2 0'");
+            string input = Console.ReadLine();
+            return input;
         }
 
         // any method is going to have
         // 1. a name
         // 2. a return value: either nothing (void), or some type e.g. int
-            // what result does the method need to send back to the code that uses this method.
+        // what result does the method need to send back to the code that uses this method.
         // 3. a list of parameters (maybe empty)
-            // does the method need any input from the code that uses it to do its job.
-        static string GetInput()
-        {
-
-        }
-
+        // does the method need any input from the code that uses it to do its job.
         static int Add(int a, int b)
         {
             int result = a + b;
