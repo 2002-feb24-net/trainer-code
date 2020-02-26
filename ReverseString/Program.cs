0f20249e3ad232db24d7839c3e9dbd3ddd5c732c
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-02-26 10:39:27 -0600

    string reverse

---
+++ ReverseString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a string: ");
+            string input = Console.ReadLine();
+            Reverse(input);
+        }
+
+        static void Reverse(string input)
+        {
+            int length = input.Length;
+            string reversedString = "";
+            for (int i = length - 1; i >= 0; i--)
+            {
+                reversedString += input[i];
+            }
+            Console.WriteLine("Reversed String: " + reversedString);
+        }
+
+        static void Reverse2(string input)
+        {
+            string exString = "abc.123.def.456";
+            char[] exCharArray = exString.ToCharArray();
+            Array.Reverse(exCharArray);
+            string revExString = String.Concat(exCharArray);
+            Console.WriteLine(revExString);
+        }
+    }
+}
