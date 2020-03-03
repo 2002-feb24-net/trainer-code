73148e188bbbd7f1754426d48f6b52ce39c04d6d
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-03-03 13:57:01 -0600

    beginning of game class

---
+++ 0b-data-structures/RockPaperScissors/RockPaperScissorsGame.cs
@@ -0,0 +1,20 @@
+namespace RockPaperScissors
+{
+    class RockPaperScissorsGame
+    {
+        // fields
+
+        // ...
+
+        // methods
+        public void PlayRound()
+        {
+            // ...
+        }
+
+        public void PrintSummary()
+        {
+            // ...
+        }
+    }
+}
