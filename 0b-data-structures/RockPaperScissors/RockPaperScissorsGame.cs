9dff1d463442109220e45f5dc82e085eb3414bdf
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-03-03 14:12:17 -0600

    begin to implement game

--- 0b-data-structures/RockPaperScissors/RockPaperScissorsGame.cs
+++ 0b-data-structures/RockPaperScissors/RockPaperScissorsGame.cs
@@ -1,20 +1,40 @@
+using System;
+
 namespace RockPaperScissors
 {
     class RockPaperScissorsGame
     {
         // fields
-
-        // ...
+        int wins = 0;
+        int losses = 0;
+        int ties = 0;
 
         // methods
         public void PlayRound()
         {
-            // ...
+            int roundNumber = wins + losses + ties + 1;
+
+            Console.Write("Round " + roundNumber + ". Enter R, P, or S: ");
+            string input = Console.ReadLine();
+
+            string computersMove = DecideMove();
+
+            // e.g... a bunch of nested if-else
+            // compare input and computersMove
         }
 
         public void PrintSummary()
         {
             // ...
+        }
+
+        string DecideMove()
+        {
+            if (losses == 0)
+            {
+                return "P";
+            }
+            return "S";
         }
     }
 }
