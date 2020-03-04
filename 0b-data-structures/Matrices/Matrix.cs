e52d5a455ff1474f85a2e2392b79067420010e76
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-03-04 14:49:28 -0600

    implement add for mutable matrix

--- 0b-data-structures/Matrices/Matrix.cs
+++ 0b-data-structures/Matrices/Matrix.cs
@@ -1,12 +1,55 @@
+using System;
+
 namespace Matrices
 {
     class Matrix
     {
         // field(s)
         // how will we store the data of this matrix
+        int[,] data;
 
         // methods
         // 1. some way to fill in the data
+
+        // erase the current data and make a new
+        // matrix with all zeroes
+        public void ClearAndSetDimension(int rows, int cols)
+        {
+            data = new int[rows, cols];
+        }
+
+        public void SetData(int row, int col, int value)
+        {
+            data[row, col] = value;
+        }
+
+        public void Add(Matrix other)
+        {
+            int rows = data.GetLength(0);
+            int cols = data.GetLength(1);
+
+            // print an error if the sizes aren't the same.
+            // (GetLength will give us one of the side lengths of a multi-d array)
+            bool error = (rows != other.data.GetLength(0) ||
+                cols != other.data.GetLength(1));
+            if (error)
+            {
+                Console.WriteLine("Incompatible matrix sizes");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    // for each row...
+                    for (int j = 0; j < cols; j++)
+                    {
+                        // for each column in that row
+                        data[i, j] += other.data[i, j];
+                    }
+                }
+            }
+        }
 
         // 2. some common matrix operations
         //    (matrix addition, matrix negation,
--- 0b-data-structures/Matrices/Program.cs
+++ 0b-data-structures/Matrices/Program.cs
@@ -9,6 +9,33 @@ namespace Matrices
             // here in the Program class,
             // you can write code to test out your
             // Matrix class
+
+            Matrix matrix = new Matrix();
+
+            // | 0 0 |
+            // | 0 0 |
+            matrix.ClearAndSetDimension(2, 2);
+
+            matrix.SetData(row: 0, col: 0, value: 2);
+            matrix.SetData(row: 0, col: 1, value: 5);
+            matrix.SetData(row: 1, col: 0, value: -1);
+            matrix.SetData(row: 1, col: 1, value: 0);
+            // |  2  5 |
+            // | -1  0 |
+
+            Matrix matrix2 = new Matrix();
+            matrix2.ClearAndSetDimension(2, 2);
+            matrix2.SetData(row: 0, col: 0, value: 1);
+            matrix2.SetData(row: 0, col: 1, value: 1);
+            matrix2.SetData(row: 1, col: 0, value: 1);
+            matrix2.SetData(row: 1, col: 1, value: 1);
+
+            // | 1 1 |
+            // | 1 1 |
+
+            matrix.Add(matrix2);
+            // | 3 6 |
+            // | 0 1 |
         }
     }
 }
