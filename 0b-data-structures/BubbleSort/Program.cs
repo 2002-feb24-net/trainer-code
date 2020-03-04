44fadebb921174bbae0f7110f385e970c5fc405b
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-03-04 16:33:12 -0600

    add skeleton code for bubble sort

---
+++ 0b-data-structures/BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 3, 4, 2, 1, 6 };
+            Console.WriteLine("before sort: [" + string.Join(",", array1) + "]");
+
+            BubbleSort(array1);
+            Console.WriteLine("after sort: [" + string.Join(",", array1) + "]");
+        }
+
+        static void BubbleSort(int[] array)
+        {
+            // 1. look at each consecutive pair,
+            //    swap them if they are not in order
+
+            // 2. do it again, until you go a whole
+            //    pass without making any swaps
+        }
+    }
+}
