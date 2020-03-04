a79672c83b75ce4421a2d6d878df52e5faf0ab9a
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-03-04 11:58:25 -0600

    add skeleton for matrix code

---
+++ 0b-data-structures/Matrices/Matrix.cs
@@ -0,0 +1,15 @@
+namespace Matrices
+{
+    class Matrix
+    {
+        // field(s)
+        // how will we store the data of this matrix
+
+        // methods
+        // 1. some way to fill in the data
+
+        // 2. some common matrix operations
+        //    (matrix addition, matrix negation,
+        //     multiplication, transpose)
+    }
+}
---
+++ 0b-data-structures/Matrices/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Matrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // here in the Program class,
+            // you can write code to test out your
+            // Matrix class
+        }
+    }
+}
