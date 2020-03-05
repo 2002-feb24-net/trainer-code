bc9899b8fa2ebcd7d6c992decb48d459c819a857
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-03-05 10:32:39 -0600

    implement bubble sort

--- 0b-data-structures/BubbleSort/Program.cs
+++ 0b-data-structures/BubbleSort/Program.cs
@@ -13,13 +13,43 @@ namespace BubbleSort
             Console.WriteLine("after sort: [" + string.Join(",", array1) + "]");
         }
 
+
+        // 1. look at each consecutive pair,
+        //    swap them if they are not in order
+
+        // e.g. this method will turn { 3 2 1 } into ... { 2 1 3 }
+        // returns true if the array was modified (i.e. it wasn't
+        // sort when we started the pass)
+        static bool BubbleSortOnePass(int[] array)
+        {
+            bool changed = false;
+            // iterate pair by pair
+            // first pair: (0 and 1)
+            // last pair: (len-2 and len-1).
+            // therefore i should start at 0 and stop before len-1
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    changed = true;
+                    // swap using a temporary variable
+                    int swap = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = swap;
+                }
+            }
+            return changed;
+        }
+
+        // 2. do it again, until you go a whole
+        //    pass without making any swaps
         static void BubbleSort(int[] array)
         {
-            // 1. look at each consecutive pair,
-            //    swap them if they are not in order
-
-            // 2. do it again, until you go a whole
-            //    pass without making any swaps
+            bool changed = BubbleSortOnePass(array);
+            while (changed)
+            {
+                changed = BubbleSortOnePass(array);
+            }
         }
     }
 }
