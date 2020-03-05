a260f56deef4da0e25648623aa8758e5a2f849ac
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-03-05 12:26:01 -0600

    fibonacci with iterative

--- 0b-data-structures/Fibonacci/Program.cs
+++ 0b-data-structures/Fibonacci/Program.cs
@@ -7,11 +7,12 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            int howMany = 40;
-            for (int i = 0; i < howMany; i++)
-            {
-                Console.Write(FibonacciIterative(i) + " ");
-            }
+            int howMany = 5;
+            FibonacciNumber(howMany);
+            // for (int i = 0; i < howMany; i++)
+            // {
+            //     Console.Write(FibonacciNumber(i) + " ");
+            // }
             Console.WriteLine("Method called " + counter + " times");
         }
 
@@ -21,15 +22,15 @@ namespace Fibonacci
         static int FibonacciNumber(int i)
         {
             // base cases
-            // if (i == 0) return 0;
-            // if (i == 1) return 1;
-            if (i < memoizedValues.Count) return memoizedValues[i];
+            if (i == 0) return 0;
+            if (i == 1) return 1;
+            // if (i < memoizedValues.Count) return memoizedValues[i];
 
             counter++;
             // recursive case
             // recursion is just a method calling itself
             int result = FibonacciNumber(i - 2) + FibonacciNumber(i - 1);
-            memoizedValues.Add(result);
+            // memoizedValues.Add(result);
             return result;
         }
 
@@ -43,7 +44,20 @@ namespace Fibonacci
         static int FibonacciIterative(int i)
         {
             // version 2, no recursion allowed
-            return 0;
+            if (i <= 1) return i; // if 0, return 0, if 1, return 1
+
+            // this array will store all the fibonacci numbers up to i.
+            int[] values = new int[i + 1];
+            // start with filling in the first two:
+            values[0] = 0;
+            values[1] = 1;
+            // fill in the rest...
+            for (int j = 2; j < values.Length; j++)
+            {
+                values[j] = values[j - 2] + values[j - 1];
+            }
+            // once values[i] is filled in,
+            return values[i];
         }
     }
 }
