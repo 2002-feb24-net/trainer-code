7a564cce8993e58397b76553ec1ddaeafb2995ae
Nicholas Escalona <nicholas.escalona@gmail.com>
2020-03-06 11:41:17 -0600

    dictionary

---
+++ 0b-data-structures/Dictionaries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameToGradeMap = new Dictionary<string, int>();
+
+            // indexing
+            nameToGradeMap["Bob"] = 91;
+
+            string[] names = { "A", "B", "C" };
+            int[] grades = { 1, 2, 3 };
+            nameToGradeMap = new Dictionary<string, int>()
+            {
+                ["Bill"] = 59
+            };
+
+            foreach (var item in nameToGradeMap)
+            {
+                string key = item.Key;
+                int value = item.Value;
+            }
+        }
+
+        // instead of using a Dictionary,
+        // we could use a class to combine those
+        // two values, and store a List of class instances.
+        class Student
+        {
+            string name;
+            int grade;
+        }
+    }
+}
