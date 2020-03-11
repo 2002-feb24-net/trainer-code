81a24d4e95272d6cef8e9a0b8606d12e7753168e
Nick Escalona <nicholas.escalona@gmail.com>
2020-03-11 18:37:47 -0500

    nick's version with the funky ctor business

---
+++ 1-csharp/MarksCodeAndPPTs/InheritanceMammalCatHumanNick/Cat.cs
@@ -0,0 +1,15 @@
+﻿namespace InheritanceMammalCatHuman
+{
+    public class Cat : Mammal
+    {
+        public Cat(int avgHeartRate)
+            : base(hasFur: true, numLegs: 4, avgHeartRate: avgHeartRate, "lungs")
+        {
+        }
+
+        public override string Says()
+        {
+            return "Meow";
+        }
+    }
+}
---
+++ 1-csharp/MarksCodeAndPPTs/InheritanceMammalCatHumanNick/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InheritanceMammalCatHuman
+{
+    class Program
+    {
+        static void Main()
+        {
+            var joey = new Human(avgHeartRate: 50, favoriteColor: "blue");
+            var garfield = new Cat(avgHeartRate: 128);
+
+            Console.WriteLine(garfield.Says());
+        }
+    }
+}
