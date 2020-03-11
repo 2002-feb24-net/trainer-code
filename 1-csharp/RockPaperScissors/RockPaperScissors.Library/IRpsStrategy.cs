2bcd776030f690c584af7c24d24771c4181eccb1
Nick Escalona <nicholas.escalona@gmail.com>
2020-03-11 15:54:30 -0500

    property

--- 1-csharp/RockPaperScissors/RockPaperScissors.Library/IRpsStrategy.cs
+++ 1-csharp/RockPaperScissors/RockPaperScissors.Library/IRpsStrategy.cs
@@ -10,6 +10,11 @@ namespace RockPaperScissors.Library
     //     (some class instance which implements it)
     public interface IRpsStrategy
     {
+        // we can have properties in interfaces
+        // this isn't an "auto property with no set"
+        // this is "you have to have some property int Successes with _at least_ a public get"
+        //int Successes { get; }
+
         string DecideMove(List<string> previousOutcomes);
     }
 }
