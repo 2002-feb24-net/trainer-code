746e7ff570263bfd2546f34e069ac87b23afef86
Nick Escalona <nicholas.escalona@gmail.com>
2020-03-11 11:18:28 -0500

    ref param example

---
+++ 1-csharp/RockPaperScissors/RockPaperScissors.App/RefParam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissors.App
+{
+    static class RefParam
+    {
+        static void UseSpecialParams()
+        {
+            int x = 10;
+            SetIntTo50(ref x);
+            // x is now 50
+
+            var list = new List<string> { "asdf" };
+            ReplaceListWithNull(ref list);
+            // now list is actually null
+        }
+
+        static void ReplaceListWithNull(ref List<string> theList)
+        {
+            theList = null;
+        }
+
+        static void SetIntTo50(ref int y)
+        {
+            y = 50;
+        }
+
+        static void SetIntTo50ButFail(int y)
+        {
+            y = 50;
+        }
+    }
+}
