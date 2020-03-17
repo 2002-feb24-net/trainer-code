38a142f75cbcab80d3a11b9a26cde6a081e75e65
Nick Escalona <nicholas.escalona@gmail.com>
2020-03-17 16:13:40 -0500

    add ef demo.

---
+++ 2-sql/EfDemo/EfDemo/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfDemo
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Person> Children { get; set; }
+    }
+}
---
+++ 2-sql/EfDemo/EfDemo/PersonContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfDemo
+{
+    // this context class represents the whole database
+    public class PersonContext : DbContext
+    {
+        // you need DbSet properties, one per table (one per type of data you want to store)
+        public DbSet<Person> Persons { get; set; }
+
+        // in order to connect to any database (or many other kinds of data source)
+        // we need a "connection string"
+
+        // we need to give each DbContext instance a connection string...
+        // one clumsy hardcoded easy one is OnConfiguring
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Data Source=../../../persons.db";
+            // need another package for this (Microsoft.EntityFrameworkCore.Sqlite)
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+}
