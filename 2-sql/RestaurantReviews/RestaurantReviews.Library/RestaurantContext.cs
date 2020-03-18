9269cb11ce4a27ab02952eb59d0e0054f2a397e9
Nick Escalona <nicholas.escalona@gmail.com>
2020-03-18 10:04:30 -0500

    add copy of rest reviews with beginnings of EF

---
+++ 2-sql/RestaurantReviews/RestaurantReviews.Library/RestaurantContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using RestaurantReviews.Library.Models;
+
+namespace RestaurantReviews.Library
+{
+    public class RestaurantContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=../../../restaurant.db");
+        }
+    }
+}
