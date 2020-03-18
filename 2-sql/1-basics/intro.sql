e06c556278aba00896d47156fa9970af3b36cd19
Nick Escalona <nicholas.escalona@gmail.com>
2020-03-18 17:11:13 -0500

    add first sql script

---
+++ 2-sql/1-basics/intro.sql
@@ -0,0 +1,20 @@
+-- we know what a relational database is
+
+-- the standard way to interact with relational database is a language called SQL
+-- (Structured Query Language)
+
+-- SQL is very declarative - meaning, you don't say HOW you want to get the data
+-- you just say the data you want, and the SQL server will calculate and plan the
+--      fastest algorithms to go and get it
+
+-- SQL variants
+--  SQLite, Oracle SQL, SQL Server, PostgreSQL
+
+-- with SQL Server, technically, SQL Server is the name of the software that runs to manage the DB.
+-- the *language* that you send is called Transact-SQL, aka T-SQL.
+
+-- RDBMS relational database management system
+
+SELECT 'Hello world!'
+
+-- 
