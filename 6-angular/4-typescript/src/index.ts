f89a8bfdd450f17656b7b3bf47f58f3a05a844e6
Nick Escalona <nicholas.escalona@gmail.com>
2020-04-15 12:11:23 -0500

    notes

--- 6-angular/4-typescript/src/index.ts
+++ 6-angular/4-typescript/src/index.ts
@@ -1,9 +1,11 @@
 // 1. create folder for this node stuff
 // 2. make package.json with `npm init -y`
-// 3. make sure typescript is globally installed so we can use tsc.
-// 4. run tsc init to get a tsconfig.json file.
-// 5. write some typescript.
-// 6. run `tsc` to convert it to JS.
+// 3. `npm install --save-dev typescript` (install typescript locally)
+// 4. run `npx tsc init` to get a tsconfig.json file.
+// 5. write some html and typescript in `src/`, make sure tsconfig.json has that folder for rootDir, and out/ for outDir.
+// 6. `npm install --save-dev webpack webpack-cli` (install webpack locally)
+// 7. create webpack.config.js based on the simple example in the docs, but with entry set to src/whateveryourentrypointis.js
+// 8. write the npm scripts in package.json... build script would be "tsc && webpack && cp src/*.html dist/"
 
 import StringHelper from "./string-helper";
 import LowercaseStringHelper from "./lowercase-string-helper";
